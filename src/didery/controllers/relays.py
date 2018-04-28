96da982f81fadab60f5c8191b8eebbe7f4e2a46e
Nick Telfer <ntelfer40@gmail.com>
2018-04-27 18:16:53 -0600

    added validation and testing to relay endpoint

--- src/didery/controllers/relays.py
+++ src/didery/controllers/relays.py
@@ -4,6 +4,87 @@ try:
 except ImportError:
     import json
 
+from ..help import helping
+
+
+def validatePost(req, resp, resource, params):
+    """
+    Validate incoming POST request and prepare
+    body of request for processing.
+    :param req: Request object
+    """
+
+    helping.parseReqBody(req)
+    body = req.body
+
+    required = ["host_address", "port", "name"]
+    helping.validateRequiredFields(required, body)
+
+    if body['host_address'] == "":
+        raise falcon.HTTPError(falcon.HTTP_400,
+                               'Malformed Field',
+                               'host_address field cannot be empty.')
+
+    if body['port'] == "":
+        raise falcon.HTTPError(falcon.HTTP_400,
+                               'Malformed Field',
+                               'port field cannot be empty.')
+
+    if body['name'] == "":
+        raise falcon.HTTPError(falcon.HTTP_400,
+                               'Malformed Field',
+                               'name field cannot be empty.')
+
+    if 'main' in body:
+        if body['main'] == "":
+            raise falcon.HTTPError(falcon.HTTP_400,
+                                   'Malformed Field',
+                                   'main field cannot be empty.')
+
+        if type(body['main']) != bool:
+            raise falcon.HTTPError(falcon.HTTP_400,
+                                   'Malformed Field',
+                                   'main field must be a boolean value.')
+
+    if 'auto' in body:
+        if body['auto'] == "":
+            raise falcon.HTTPError(falcon.HTTP_400,
+                                   'Malformed Field',
+                                   'auto field cannot be empty.')
+
+        if type(body['auto']) != bool:
+            raise falcon.HTTPError(falcon.HTTP_400,
+                                   'Malformed Field',
+                                   'auto field must be a boolean value.')
+
+    try:
+        int(body['port'])
+    except ValueError:
+        raise falcon.HTTPError(falcon.HTTP_400,
+                               'Malformed Field',
+                               'port field must be a number.')
+
+    if int(body['port']) < 1 or int(body['port']) > 65535:
+        raise falcon.HTTPError(falcon.HTTP_400,
+                               'Malformed Field',
+                               'port field must be a number between 1 and 65535.')
+
+
+def validatePut(req, resp, resource, params):
+    validatePost(req, resp, resource, params)
+
+    if "uid" not in params:
+        raise falcon.HTTPError(falcon.HTTP_400,
+                               'Incomplete Request',
+                               'uid required.')
+
+
+def validateDelete(req, resp, resource, params):
+    if "uid" not in params:
+        raise falcon.HTTPError(falcon.HTTP_400,
+                               'Incomplete Request',
+                               'uid required.')
+
 
 class Relay:
     def __init__(self, store=None):
@@ -50,26 +131,14 @@ class Relay:
     For manual testing of the endpoint:
         http POST localhost:8000/relay host_address="127.0.0.1" port=7541 name="alpha" main=true auto=true
     """
+    @falcon.before(validatePost)
     def on_post(self, req, resp):
         """
         Handle and respond to incoming POST request.
         :param req: Request object
         :param resp: Response object
         """
-        try:
-            raw_json = req.stream.read()
-        except Exception as ex:
-            raise falcon.HTTPError(falcon.HTTP_400,
-                                   'Error',
-                                   'Error reading request body.')
-
-        try:
-            result_json = json.loads(raw_json, encoding='utf-8')
-        except ValueError:
-            raise falcon.HTTPError(falcon.HTTP_400,
-                                   'Malformed JSON',
-                                   'Could not decode the request body. The '
-                                   'JSON was incorrect.')
+        result_json = req.body
 
         result_json["uid"] = "1"
         result_json["status"] = "connected"
@@ -80,26 +149,15 @@ class Relay:
     For manual testing of the endpoint:
         http PUT localhost:8000/relay/1 host_address="127.0.0.1" port=7541 name="alpha" main=true auto=true
     """
+    @falcon.before(validatePut)
     def on_put(self, req, resp, uid):
         """
         Handle and respond to incoming PUT request.
         :param req: Request object
         :param resp: Response object
+        :param uid: servers unique id
         """
-        try:
-            raw_json = req.stream.read()
-        except Exception as ex:
-            raise falcon.HTTPError(falcon.HTTP_400,
-                                   'Error',
-                                   'Error reading request body.')
-
-        try:
-            result_json = json.loads(raw_json, encoding='utf-8')
-        except ValueError:
-            raise falcon.HTTPError(falcon.HTTP_400,
-                                   'Malformed JSON',
-                                   'Could not decode the request body. The '
-                                   'JSON was incorrect.')
+        result_json = req.body
 
         result_json["uid"] = uid
         result_json["status"] = "connected"
@@ -110,11 +168,13 @@ class Relay:
     For manual testing of the endpoint:
         http DELETE localhost:8000/relay/10
     """
+    @falcon.before(validateDelete)
     def on_delete(self, req, resp, uid):
         """
         Handle and respond to incoming DELETE request.
         :param req: Request object
         :param resp: Response object
+        :param uid: servers unique id
         """
         body = {
             "host_address": "127.0.0.1",
