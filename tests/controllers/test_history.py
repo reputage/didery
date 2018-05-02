3316f2c4252dee758df591762bedc5d982acfa29
Nick Telfer <ntelfer40@gmail.com>
2018-05-02 11:48:48 -0600

    added signature headers to remaining failing tests.

--- tests/controllers/test_history.py
+++ tests/controllers/test_history.py
@@ -9,7 +9,21 @@ from didery.help import helping
 
 
 SK = b"\xb3\xd0\xbdL]\xcc\x08\x90\xa5\xbd\xc6\xa1 '\x82\x9c\x18\xecf\xa6x\xe2]Ux\xa5c\x0f\xe2\x86*\xa04\xe7\xfaf\x08o\x18\xd6\xc5s\xfc+\xdc \xb4\xb4\xa6G\xcfZ\x96\x01\x1e%\x0f\x96\x8c\xfa-3J<"
-DID = b"did:igo:NOf6ZghvGNbFc_wr3CC0tKZHz1qWAR4lD5aM-i0zSjw="
+VK = b"NOf6ZghvGNbFc_wr3CC0tKZHz1qWAR4lD5aM-i0zSjw="
+
+
+def runPostTest(client, body, exp_result=None, exp_status=None):
+    headers = {
+        "Signature": 'signer="' + helping.signResource(body, SK) + '"; rotation="' + helping.signResource(body,
+                                                                                                          SK) + '"'
+    }
+
+    response = client.simulate_post(HISTORY_BASE_PATH, body=body, headers=headers)
+
+    if exp_status is not None:
+        assert response.status == exp_status
+    if exp_result is not None:
+        assert response.content == exp_result
 
 
 def testPostSignValidation(client):
@@ -79,13 +93,7 @@ def testValidPostSignature(client):
            b']' \
            b'}'
 
-    headers = {
-        "Signature": 'signer="' + helping.signResource(body, SK) + '"; rotation="' + helping.signResource(body, SK) + '"'
-    }
-
-    response = client.simulate_post(HISTORY_BASE_PATH, body=body, headers=headers)
-    print(response.content)
-    assert response.status == falcon.HTTP_200
+    runPostTest(client, body, None, falcon.HTTP_200)
     # TODO:
     # assert response.content == exp_result
 
@@ -103,13 +111,11 @@ def testPostValidation(client):
             b'    "3syVH2woCpOvPF0SD9Z0bu_OxNe2ZgxKjTQ961LlMnA=" ' \
             b']' \
             b'}'
-    response = client.simulate_post(HISTORY_BASE_PATH, body=body)
 
     exp_result = b'{"title": "Missing Required Field", "description": ' \
                  b'"Request must contain id field."}'
 
-    assert response.status == falcon.HTTP_400
-    assert response.content == exp_result
+    runPostTest(client, body, exp_result, falcon.HTTP_400)
 
     # Test missing changed field
     body = b'{"id": "did:dad:Qt27fThWoNZsa88VrTkep6H-4HA8tr54sHON1vWl6FE=", ' \
@@ -122,13 +128,11 @@ def testPostValidation(client):
            b'    "3syVH2woCpOvPF0SD9Z0bu_OxNe2ZgxKjTQ961LlMnA=" ' \
            b']' \
            b'}'
-    response = client.simulate_post(HISTORY_BASE_PATH, body=body)
 
     exp_result = b'{"title": "Missing Required Field", "description": ' \
                  b'"Request must contain changed field."}'
 
-    assert response.status == falcon.HTTP_400
-    assert response.content == exp_result
+    runPostTest(client, body, exp_result, falcon.HTTP_400)
 
     # Test missing signer field
     body = b'{"id": "did:dad:Qt27fThWoNZsa88VrTkep6H-4HA8tr54sHON1vWl6FE=", ' \
@@ -141,26 +145,22 @@ def testPostValidation(client):
            b'    "3syVH2woCpOvPF0SD9Z0bu_OxNe2ZgxKjTQ961LlMnA=" ' \
            b']' \
            b'}'
-    response = client.simulate_post(HISTORY_BASE_PATH, body=body)
 
     exp_result = b'{"title": "Missing Required Field", "description": ' \
                  b'"Request must contain signer field."}'
 
-    assert response.status == falcon.HTTP_400
-    assert response.content == exp_result
+    runPostTest(client, body, exp_result, falcon.HTTP_400)
 
     # Test missing signers field
     body = b'{"id": "did:dad:Qt27fThWoNZsa88VrTkep6H-4HA8tr54sHON1vWl6FE=", ' \
            b'"changed": "2000-01-01T00:00:00+00:00", ' \
            b'"signer": 2' \
            b'}'
-    response = client.simulate_post(HISTORY_BASE_PATH, body=body)
 
     exp_result = b'{"title": "Missing Required Field", "description": ' \
                  b'"Request must contain signers field."}'
 
-    assert response.status == falcon.HTTP_400
-    assert response.content == exp_result
+    runPostTest(client, body, exp_result, falcon.HTTP_400)
 
     # Test empty id field
     body = b'{"id": "", ' \
@@ -174,13 +174,11 @@ def testPostValidation(client):
            b'    "3syVH2woCpOvPF0SD9Z0bu_OxNe2ZgxKjTQ961LlMnA=" ' \
            b']' \
            b'}'
-    response = client.simulate_post(HISTORY_BASE_PATH, body=body)
 
     exp_result = b'{"title": "Malformed Field", "description": ' \
                  b'"id field cannot be empty."}'
 
-    assert response.status == falcon.HTTP_400
-    assert response.content == exp_result
+    runPostTest(client, body, exp_result, falcon.HTTP_400)
 
     # Test empty changed field
     body = b'{"id": "did:dad:Qt27fThWoNZsa88VrTkep6H-4HA8tr54sHON1vWl6FE=", ' \
@@ -194,13 +192,11 @@ def testPostValidation(client):
            b'    "3syVH2woCpOvPF0SD9Z0bu_OxNe2ZgxKjTQ961LlMnA=" ' \
            b']' \
            b'}'
-    response = client.simulate_post(HISTORY_BASE_PATH, body=body)
 
     exp_result = b'{"title": "Malformed Field", "description": ' \
                  b'"changed field cannot be empty."}'
 
-    assert response.status == falcon.HTTP_400
-    assert response.content == exp_result
+    runPostTest(client, body, exp_result, falcon.HTTP_400)
 
     # Test invalid signers value
     body = b'{"id": "did:dad:Qt27fThWoNZsa88VrTkep6H-4HA8tr54sHON1vWl6FE=", ' \
@@ -208,13 +204,11 @@ def testPostValidation(client):
            b'"signer": 2, ' \
            b'"signers": ""' \
            b'}'
-    response = client.simulate_post(HISTORY_BASE_PATH, body=body)
 
     exp_result = b'{"title": "Malformed Field", "description": ' \
                  b'"signers field must be a list or array."}'
 
-    assert response.status == falcon.HTTP_400
-    assert response.content == exp_result
+    runPostTest(client, body, exp_result, falcon.HTTP_400)
 
     # Test that signers field has two keys
     body = b'{"id": "did:dad:Qt27fThWoNZsa88VrTkep6H-4HA8tr54sHON1vWl6FE=", ' \
@@ -225,39 +219,36 @@ def testPostValidation(client):
            b'    "Qt27fThWoNZsa88VrTkep6H-4HA8tr54sHON1vWl6FE="' \
            b']' \
            b'}'
-    response = client.simulate_post(HISTORY_BASE_PATH, body=body)
 
     exp_result = b'{"title": "Malformed Field", "description": ' \
                  b'"signers field must contain at least the current public key and its first pre-rotation."}'
 
-    assert response.status == falcon.HTTP_400
-    assert response.content == exp_result
+    runPostTest(client, body, exp_result, falcon.HTTP_400)
 
+    # Test that signers field has two keys
     body = b'{"id": "did:dad:Qt27fThWoNZsa88VrTkep6H-4HA8tr54sHON1vWl6FE=", ' \
            b'"changed": "2000-01-01T00:00:00+00:00", ' \
            b'"signer": 2, ' \
            b'"signers": ' \
            b'[]' \
            b'}'
-    response = client.simulate_post(HISTORY_BASE_PATH, body=body)
 
     exp_result = b'{"title": "Malformed Field", "description": ' \
                  b'"signers field must contain at least the current public key and its first pre-rotation."}'
 
-    assert response.status == falcon.HTTP_400
-    assert response.content == exp_result
+    runPostTest(client, body, exp_result, falcon.HTTP_400)
 
+    # Test that signers field has two keys
     body = b'{"id": "did:dad:Qt27fThWoNZsa88VrTkep6H-4HA8tr54sHON1vWl6FE=", ' \
            b'"changed": "2000-01-01T00:00:00+00:00", ' \
            b'"signer": 0, ' \
            b'"signers": ' \
            b'[' \
-           b'    "Qt27fThWoNZsa88VrTkep6H-4HA8tr54sHON1vWl6FE=", ' \
-           b'    "Xq5YqaL6L48pf0fu7IUhL0JRaU2_RxFP0AL43wYn148="' \
+           b'    "NOf6ZghvGNbFc_wr3CC0tKZHz1qWAR4lD5aM-i0zSjw=", ' \
+           b'    "NOf6ZghvGNbFc_wr3CC0tKZHz1qWAR4lD5aM-i0zSjw="' \
            b']' \
            b'}'
-    response = client.simulate_post(HISTORY_BASE_PATH, body=body)
-    assert response.status == falcon.HTTP_200
+    runPostTest(client, body, None, falcon.HTTP_200)
 
     # Test that signer field is an int
     body = b'{"id": "did:dad:Qt27fThWoNZsa88VrTkep6H-4HA8tr54sHON1vWl6FE=", ' \
@@ -271,13 +262,11 @@ def testPostValidation(client):
            b'    "3syVH2woCpOvPF0SD9Z0bu_OxNe2ZgxKjTQ961LlMnA=" ' \
            b']' \
            b'}'
-    response = client.simulate_post(HISTORY_BASE_PATH, body=body)
 
     exp_result = b'{"title": "Malformed Field", "description": ' \
                  b'"signer field must be a number."}'
 
-    assert response.status == falcon.HTTP_400
-    assert response.content == exp_result
+    runPostTest(client, body, exp_result, falcon.HTTP_400)
 
     # Test that signer field is a valid index into signers field
     body = b'{"id": "did:dad:Qt27fThWoNZsa88VrTkep6H-4HA8tr54sHON1vWl6FE=", ' \
@@ -291,13 +280,11 @@ def testPostValidation(client):
            b'    "3syVH2woCpOvPF0SD9Z0bu_OxNe2ZgxKjTQ961LlMnA=" ' \
            b']' \
            b'}'
-    response = client.simulate_post(HISTORY_BASE_PATH, body=body)
 
     exp_result = b'{"title": "Malformed Field", "description": ' \
                  b'"signer field must be between 0 and size of signers field."}'
 
-    assert response.status == falcon.HTTP_400
-    assert response.content == exp_result
+    runPostTest(client, body, exp_result, falcon.HTTP_400)
 
     # Test that there is a pre-rotated key included in signers field
     body = b'{"id": "did:dad:Qt27fThWoNZsa88VrTkep6H-4HA8tr54sHON1vWl6FE=", ' \
@@ -311,13 +298,11 @@ def testPostValidation(client):
            b'    "3syVH2woCpOvPF0SD9Z0bu_OxNe2ZgxKjTQ961LlMnA=" ' \
            b']' \
            b'}'
-    response = client.simulate_post(HISTORY_BASE_PATH, body=body)
 
     exp_result = b'{"title": "Malformed Field", "description": ' \
                  b'"Missing pre rotated key in the signers field."}'
 
-    assert response.status == falcon.HTTP_400
-    assert response.content == exp_result
+    runPostTest(client, body, exp_result, falcon.HTTP_400)
 
 
 def testValidPost(client):
@@ -328,12 +313,11 @@ def testValidPost(client):
            b'[' \
            b'    "Qt27fThWoNZsa88VrTkep6H-4HA8tr54sHON1vWl6FE=", ' \
            b'    "Xq5YqaL6L48pf0fu7IUhL0JRaU2_RxFP0AL43wYn148=", ' \
-           b'    "dZ74MLZXD-1QHoa73w9pQ9GroAvxqFi2RTZWlkC0raY=", ' \
-           b'    "3syVH2woCpOvPF0SD9Z0bu_OxNe2ZgxKjTQ961LlMnA=" ' \
+           b'    "NOf6ZghvGNbFc_wr3CC0tKZHz1qWAR4lD5aM-i0zSjw=", ' \
+           b'    "NOf6ZghvGNbFc_wr3CC0tKZHz1qWAR4lD5aM-i0zSjw=" ' \
            b']' \
            b'}'
 
-    response = client.simulate_post(HISTORY_BASE_PATH, body=body)
-    assert response.status == falcon.HTTP_200
+    runPostTest(client, body, None, falcon.HTTP_200)
     # TODO:
     # assert response.content == expected_response
