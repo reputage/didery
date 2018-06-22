f8e1c41a7d110a331d5236bd723c0ac3db7047fa
Nick Telfer <ntelfer40@gmail.com>
2018-06-21 22:48:00 -0600

    updated some comments

--- src/didery/cli.py
+++ src/didery/cli.py
@@ -6,11 +6,11 @@ Why does this file exist, and why not put this in __main__?
   You might be tempted to import things from __main__ later, but that will cause
   problems: the code will get executed twice:
 
-  - When you run `python -mbluepea` python will execute
+  - When you run `python -m dideryd` python will execute
     ``__main__.py`` as a script. That means there won't be any
-    ``bluepea.__main__`` in ``sys.modules``.
+    ``didery.__main__`` in ``sys.modules``.
   - When you import __main__ it will get executed again (as a module) because
-    there's no ``bluepea.__main__`` in ``sys.modules``.
+    there's no ``didery.__main__`` in ``sys.modules``.
 
   Also see (1) from http://click.pocoo.org/5/setuptools/#setuptools-integration
 """
